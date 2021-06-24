ef913e16ff3fdea24cafe0408da6e748f9ba281a
Kabra <abhinav.kabra@accenture.com>
2021-06-24 16:39:55 -0400

    Completed module

--- main.go
+++ main.go
@@ -33,9 +33,9 @@ func main() {
 		}
 
 		if originUnit == "C" {
-			convertToCelsius(originValue)
-		} else {
 			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
 		}
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 		_, err = fmt.Scanln(&shouldConvertAgain)
